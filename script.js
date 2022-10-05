288daeb54a1921953933a73ecdac1692ebbd576f
Keadon Mitchell <mitchell.keadon@outlook.com>
2022-10-05 15:08:16 -0500

    Add calculation functionality

--- script.js
+++ script.js
@@ -1,32 +1,78 @@
-let operand1;
-let operand2;
-let operator;
+let operator = "";
+let num1 = "";
+let num2 = "";
+let answer = "";
 
-const screen = document.querySelector(".screen");
+const mainDisplay = document.querySelector(".main-display");
+const historyDisplay = document.querySelector(".history-display");
 const equals = document.querySelector(".equals");
 const dot = document.querySelector(".dot");
 const clear = document.querySelector(".clear");
+const clearEntry = document.querySelector(".clear-entry");
 const operators = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".number");
 
-operators.forEach((opButton) => {});
+equals.onclick = () => displayAnswer();
+clear.onclick = () => clearDisplay();
 
-numbers.forEach((number) => {});
+/*Inputs*/
+operators.forEach((opButton) => {
+  opButton.addEventListener("click", (e) => {
+    if (!num1) num1 = "0";
+    operator = e.target.getAttribute("data-operator");
+    updateMainDisplay();
+  });
+});
 
-function calculate() {
-  const answer = operate(operand1, operand2, operator);
-  updateDisplay("= " + answer);
+numbers.forEach((numButton) =>
+  numButton.addEventListener("click", (e) => {
+    updateOperands(e.target.getAttribute("data-num"));
+  })
+);
+
+function updateOperands(num) {
+  if (num === "0") {
+    //Stops user from entering 0 first
+    if (!num1) return;
+    if (!num2) return;
+  }
+
+  if (!operator) {
+    //Selects num1 if the operator hasn't been selected
+    num1 += num;
+  } else {
+    num2 += num;
+  }
+  updateMainDisplay();
 }
 
-function updateDisplay(content) {
-  screen.textContent = screen.textContent + " " + content;
+/*Display*/
+function updateMainDisplay() {
+  mainDisplay.textContent = `${num1} ${operator} ${num2}`;
+}
+
+function displayAnswer() {
+  const answer = operate(parseFloat(num1), parseFloat(num2));
+  if (answer === "ERROR" || !answer) return;
+  updateHistoryDisplay();
+  mainDisplay.textContent = `${answer}`;
+  num1 = answer;
+  num2 = "";
+  operator = "";
+}
+
+function updateHistoryDisplay() {
+  // historyDisplay.textContent =
+  historyDisplay.textContent = mainDisplay.textContent + " =";
 }
 
 function clearDisplay() {
-  screen.textContent = "";
+  mainDisplay.textContent = "";
 }
 
-function operate(num1, num2, operator) {
+/*Calculate*/
+
+function operate(num1, num2) {
   switch (operator) {
     case "+":
       return num1 + num2;
@@ -34,11 +80,14 @@ function operate(num1, num2, operator) {
     case "-":
       return num1 - num2;
       break;
-    case "*":
+    case "x":
       return num1 * num2;
       break;
     case "/":
       return num1 / num2;
+      break;
+    default:
+      return "ERROR";
       break;
   }
 }
